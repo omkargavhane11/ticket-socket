f8f80f4fc95bc4e5ae20a47f4cfde41d290f05eb
Omkar Gavhane <126314263+ogomkar@users.noreply.github.com>
2024-02-04 21:00:29 +0530

    change client origin

--- index.js
+++ index.js
@@ -1,57 +1,49 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-
 const io = require("socket.io")(process.env.PORT, {
-    cors: {
-        origin: "https://myticket77.netlify.app"
-    }
+  cors: {
+    origin: process.env.CLIENT,
+  },
 });
 
 let users = [];
 
 const addUser = (userId, socketId) => {
-    !users.some((user) => user.userId === userId) && users.push({ userId, socketId });
-
-}
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
 
 const removeUser = (socketId) => {
-    users = users.filter((user) => user.socketId !== socketId)
-}
+  users = users.filter((user) => user.socketId !== socketId);
+};
 
 const getUser = (userId) => {
-    let user = users.find((user) => user.userId === userId);
-    return user;
-}
+  let user = users.find((user) => user.userId === userId);
+  return user;
+};
 
 io.on("connection", (socket) => {
-    console.log("user connected " + socket.id);
+  console.log("user connected " + socket.id);
 
-    socket.on("addUser", userId => {
-        addUser(userId, socket.id)
-        io.emit("getUsers", users)
-    })
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
 
-    socket.on("disconnect", () => {
-        console.log("user disconnected " + socket.id);
-        removeUser(socket.id);
-        io.emit("getUsers", users)
-    })
+  socket.on("disconnect", () => {
+    console.log("user disconnected " + socket.id);
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
 
-    socket.on("sendMessage", ({
-        senderId,
-        recieverId,
-        queryId,
-        message,
-    }) => {
-        let user = getUser(recieverId);
+  socket.on("sendMessage", ({ senderId, recieverId, queryId, message }) => {
+    let user = getUser(recieverId);
 
-        io.to(user?.socketId).emit("getMessage", {
-            senderId, message, queryId
-        })
-    })
-
-})
-
-
-
+    io.to(user?.socketId).emit("getMessage", {
+      senderId,
+      message,
+      queryId,
+    });
+  });
+});
